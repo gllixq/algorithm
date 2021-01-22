364c4a25fd4bf8ed2cecdfd35729191eaeb49138
gllixq <gllixq@163.com>
2021-01-23 01:50:45 +0800

    插入排序

--- sort.py
+++ sort.py
@@ -1,3 +1,5 @@
+#-*- coding:utf-8 -*-
+#  冒泡排序
 def bubble_sort(li):
     for i in range(len(li) - 1):
         for j in range(len(li) - i - 1):
@@ -6,18 +8,29 @@ def bubble_sort(li):
         print li
 
 
+# 选择排序
 def select_sort(li):
-    for i in range(len(li)):
-        min_val = li[i]
+    for i in range(len(li) - 1):
         min_index = i
-        for j in range(i + 1, len(li) - i):
-            if min_val > li[j]:
-                min_val = li[j]
+        for j in range(i + 1, len(li)):
+            if li[min_index] > li[j]:
                 min_index = j
-        li[min_index] = li[i]
-        li[i] = min_val
+        li[min_index], li[i] = li[i], li[min_index]
 
 
-li = [9, 8, 7, 6, 5, 4, 3, 2, 1]
-select_sort(li)
-print li
+# 插入排序
+def insert_sort(li):
+    if len(li) == 1:
+        return
+    for i in range(0, len(li) - 1):
+        j = i + 1
+        while j > 0:
+            if li[j] < li[j - 1]:
+                li[j], li[j - 1] = li[j - 1], li[j]
+                j = j - 1
+            else:
+                break
+
+
+li = [8, 9, 7, 6, 5, 4, 3, 2, 1]
+insert_sort(li)
