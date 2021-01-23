be2c4d9bed251e03762a62dc24255f9749bf3fdd
gllixq <gllixq@163.com>
2021-01-24 03:15:56 +0800

    堆排序

--- sort.py
+++ sort.py
@@ -55,6 +55,49 @@ def quick_sort(li, left, right):
         quick_sort(li, mid + 1, right)
 
 
+# 堆排序(非递归实现)
+# 自顶向下调整函数
+def shif(li, node_index, max_index):
+    temp_val = li[node_index] # 需要调整的根节点
+    left_child_index = 2 * node_index + 1
+    while left_child_index <= max_index:
+        cur_max_index = left_child_index
+        if left_child_index + 1 <= max_index and li[left_child_index] < li[left_child_index + 1]:
+            cur_max_index = left_child_index + 1
+        if li[cur_max_index] > temp_val:
+            li[node_index], li[cur_max_index] = li[cur_max_index], li[node_index]
+            node_index = left_child_index
+            left_child_index = 2 * left_child_index + 1
+        else:
+            break
+
+
+def shif_digui(li, low, high):
+    temp_val = li[low]
+    left_child_index = 2 * low + 1
+    right_child_index = 2 * low + 2
+    if left_child_index > high:
+        return
+    max_index = left_child_index
+    if right_child_index <= high and li[right_child_index] > li[left_child_index]:
+        max_index = right_child_index
+    if temp_val < li[max_index]:
+        li[low], li[max_index] = li[max_index], li[low]
+        shif_digui(li, max_index, high)
+
+
+def heap_sort(li):
+    # 构造大根堆
+    end_parent_inex = (len(li) - 2) // 2 # 最后一个根节点的index,先从最底部调整，保证调整过后的子二叉树都符合大根堆的性质
+    for i in range(end_parent_inex, -1, -1):
+        shif_digui(li, i, len(li) - 1)
+
+    # 交换堆顶元素和最后一个叶子节点，然后对剩余元素进行堆构造
+    for i in range(len(li) - 1, 0, -1):
+        li[0], li[i] = li[i], li[0]
+        shif_digui(li, 0, i - 1)
+
+
 li = [8, 9, 7, 6, 5, 4, 3, 2, 1]
-quick_sort(li, 0, len(li) - 1)
+heap_sort(li)
 print li
